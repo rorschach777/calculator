ee6145dd273e9ecb7f909a7be52f6a95fd39dddf
rorschach777 <msweitzer1987@gmail.com>
2019-06-14 01:04:35 -0400

    this is a first commit

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import CalcButton from './Components/CalcButton';
+import ActionButton from './Components/ActionButton';
+import './App.css';
+import * as math from 'mathjs'
+
+class App extends Component {
+  state = {
+    value: ' ', 
+    defaultZero: 0
+  }
+  valueString = (passedValue) => {
+    this.setState ({
+      value: this.state.value + passedValue,
+      defaultZero: null
+    })
+  }
+  clearCalc = () =>{
+    this.setState({
+      value: '',
+      defaultZero: 0
+    })
+  }
+
+  handleEqual = () => {
+    this.setState({value: math.eval(this.state.value)});
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className='calc-wrapper'>
+            <div className='output-screen'>
+              {this.state.defaultZero}
+              {this.state.value}
+            </div>
+            <div className='row'>
+              <CalcButton insertValue={this.valueString}>7</CalcButton>
+              <CalcButton insertValue={this.valueString}>8</CalcButton>
+              <CalcButton insertValue={this.valueString}>9</CalcButton>
+              <CalcButton insertValue={this.valueString}>*</CalcButton>
+            </div>
+            <div className='row'>
+              <CalcButton insertValue={this.valueString}>5</CalcButton>
+              <CalcButton insertValue={this.valueString}>4</CalcButton>
+              <CalcButton insertValue={this.valueString}>6</CalcButton>
+              <CalcButton insertValue={this.valueString}>/</CalcButton>
+            </div>
+            <div className='row'>
+              <CalcButton insertValue={this.valueString}>1</CalcButton>
+              <CalcButton insertValue={this.valueString}>2</CalcButton>
+              <CalcButton insertValue={this.valueString}>3</CalcButton>
+              <CalcButton insertValue={this.valueString}>+</CalcButton>
+            </div>
+            <div className='row'>
+              <CalcButton  insertValue={this.valueString} className='zero'>0</CalcButton>
+              <CalcButton insertValue={this.valueString} >.</CalcButton>
+              <CalcButton insertValue={this.valueString} >-</CalcButton>
+            </div>
+            <div className='row'>
+              <ActionButton action={this.clearCalc}>AC</ActionButton>
+              <ActionButton action={this.handleEqual}>=</ActionButton>
+            </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/CalcButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CalcButton.css';
+const buttonType = (x) => {
+    return ;
+}
+
+const CalcButton = (props) => (<div 
+className={`calc-button ${props.children == 0 ?  'zero': null }`}
+onClick={()=> props.insertValue(props.children)}>
+    {props.children}
+</div>);
+ 
+export default CalcButton;
